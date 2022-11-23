36e2b3c9f0b45f1d7a097a933ec1c11055b1508b
vishnuvardhanu <117962613+vishnuvardhanu@users.noreply.github.com>
2022-11-23 20:19:12 +0530

    Create Pattern03.java

---
+++ Pattern03.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+      int n;
+
+	    Scanner inp = new Scanner(System.in);
+
+	    System.out.println("enter any number");
+
+	    n=inp.nextInt();
+
+	    for(int i=1;i<=n;i++)
+
+	    {
+
+	        for(int j=1;j<=i;j++)
+
+	        {
+
+	            System.out.print(j);	          
+
+	        }System.out.println();	        
+
+	    }	    	    
+
+	}
+
+}
